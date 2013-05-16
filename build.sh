bae9f2f559669d4fc331cdd0d5a62476074726ef
Planetguy <apricefrench2d@gmail.com>
2013-05-15 21:26:48 -0700

    Made build script take everything except MCP files, not only Gizmos files. Now it's useful for other people too (maybe).

--- build.sh
+++ build.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
+shopt -s extglob
 rm -rf reobf/minecraft
 rm -f build.jar
 python runtime/reobfuscate.py --srgnames "$@"
-cp -r src/minecraft/mods reobf/minecraft/mods
-cp -r src/minecraft/planetguy reobf/minecraft
-cp src/minecraft/*.md reobf/minecraft
-cp src/minecraft/mcmod.info reobf/minecraft
-cp -r src/minecraft/textures reobf/minecraft/textures
+cp  -r src/minecraft/!(net*|cpw*|mcp|*forge*|*fml*|*ibxm*|paulscode*|*.cfg|Start.java|*mcp*) reobf/minecraft
+cp  -r src/minecraft/!(net*|cpw*|mcp|*forge*|*fml*|*ibxm*|paulscode*|*.cfg|Start.java|*mcp*) repo/Gizmos
 
-cp -r src/minecraft/mods repo/Gizmos
-cp -r src/minecraft/planetguy repo/Gizmos
-cp src/minecraft/*.md repo/Gizmos
-cp src/minecraft/mcmod.info repo/Gizmos
-cp -r src/minecraft/textures repo/Gizmos
+rm -rf repo/Gizmos/ibxm
 
 cd reobf/minecraft
+rm -rf ixbm/
 zip -r build.jar *
 rm -f ~/Dropbox/Public/modstuff/gizmos_latest.jar
 cp build.jar ~/Dropbox/Public/modstuff/gizmos_latest.jar
